9aa21b95d4fe4952ff7924bc2e59e5c1c44cae72
fawzanoachira <fawzanoachira@gmail.com>
2026-01-17 08:26:26 +0530

    app launcher name changed

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Laebun va Lahv',
+      title: 'Lahv',
       theme: ThemeData.dark(useMaterial3: true).copyWith(
           scaffoldBackgroundColor: scaffoldColor,
           colorScheme: const ColorScheme.dark(
