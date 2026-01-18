da7afd968be426b7de566bba4488a79549ad6d4f
fawzanoachira <fawzanoachira@gmail.com>
2026-01-18 22:00:23 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lahv/app.dart';
+import 'package:lahv/firebase_options.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
